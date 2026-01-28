387cad125689bd2ee5e0b06c06f630542a722fc4
lekshmi0072014-beep <>
2026-01-28 16:30:36 +0000

    Modifying Functions

---
+++ functions/modifying_functions/main.py
@@ -0,0 +1,21 @@
+def apply_discount(price, discount=0.05):
+    discounted = price * (1 - discount)
+    return discounted
+
+
+def apply_tax(price, tax=0.07):
+    taxed = price * (1 + tax)
+    return taxed
+
+def calculate_total(price, discount=0.05, tax=0.07):
+    discounted = apply_discount(price, discount)
+    total = apply_tax(discounted, tax)
+    return total
+
+total_price_default = calculate_total(120)
+print(f"Total cost with default discount and tax: ${total_price_default}")
+total_price_custom = calculate_total(100, discount=0.10, tax=0.08)
+print(f"Total cost with custom discount and tax: ${total_price_custom}")
+
+    
+    
